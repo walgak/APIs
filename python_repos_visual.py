61c92f3bc6d2ce14fef3f6270fe37a1f937f2434
walgak <118623263+walgak@users.noreply.github.com>
2024-06-05 01:25:34 +0300

    visualising top python repositories on github

---
+++ python_repos_visual.py
@@ -0,0 +1,40 @@
+import requests
+
+#Make an API call and check the response.
+url = "https://api.github.com/search/repositories"
+url += "?q=language:python+sort:stars+stars:>10000"
+
+headers = {"Accept": "application/vnd.github.v3+json"}
+r = requests.get(url, headers = headers)
+print(f"Status code: {r.status_code}")
+
+#Convert the response object to a dictionary.
+response_dic = r.json()
+
+#Process results.
+print(response_dic.keys())
+
+#Working with the response dictionary
+print(f"Total repositories: {response_dic['total_count']}")
+print(f"Complete results: {not response_dic['incomplete_results']}")
+
+#Exploring information about the repositories.
+repo_dicts = response_dic['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+#Examine the first repository
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
+
+print(f"\nSelected information anout first repository:")
+
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict["name"]}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f'Created: {repo_dict['created_at']}')
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f'Description: {repo_dict['description']}')
