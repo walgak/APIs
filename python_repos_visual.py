56c3127bfd774430819e9de568edfe6b6d7887d3
walgak <118623263+walgak@users.noreply.github.com>
2024-06-05 02:07:59 +0300

    added links

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -1,4 +1,5 @@
 import requests
+import plotly.express as px
 
 #Make an API call and check the response.
 url = "https://api.github.com/search/repositories"
@@ -11,30 +12,35 @@ print(f"Status code: {r.status_code}")
 #Convert the response object to a dictionary.
 response_dic = r.json()
 
-#Process results.
+#Process overall results.
 print(response_dic.keys())
 
 #Working with the response dictionary
 print(f"Total repositories: {response_dic['total_count']}")
 print(f"Complete results: {not response_dic['incomplete_results']}")
 
-#Exploring information about the repositories.
+#Process repository information.
 repo_dicts = response_dic['items']
-print(f"Repositories returned: {len(repo_dicts)}")
-
-#Examine the first repository
-repo_dict = repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")
-for key in sorted(repo_dict.keys()):
-    print(key)
-
-print(f"\nSelected information anout first repository:")
-
+repo_links, stars, hover_texts = [],[], []
 for repo_dict in repo_dicts:
-    print(f"Name: {repo_dict["name"]}")
-    print(f"Owner: {repo_dict['owner']['login']}")
-    print(f"Stars: {repo_dict['stargazers_count']}")
-    print(f"Repository: {repo_dict['html_url']}")
-    print(f'Created: {repo_dict['created_at']}')
-    print(f"Updated: {repo_dict['updated_at']}")
-    print(f'Description: {repo_dict['description']}')
+    #Turn repo names to active links
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href = '{repo_url}'>{repo_name}"
+    repo_links.append(repo_link)
+
+    stars.append(repo_dict['stargazers_count'])
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    hover_text = f"{owner}<br />{description}"
+    hover_texts.append(hover_text)
+
+#Make a visualisation
+title = "Most-Starred Python Projects on Github"
+labels = {'x': 'Repository', 'y': 'Stars'}
+fig = px.bar(x = repo_links, y = stars, title = title, labels = labels, 
+             hover_name = hover_texts)
+fig.update_layout(title_font_size = 28, xaxis_title_font_size = 20, 
+                  yaxis_title_font_size = 20)
+fig.update_traces(marker_color = 'SteelBlue', marker_opacity = 0.6)
+fig.show()
